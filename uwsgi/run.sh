cc791ff1ed34bc9f36f92bff83946ff21a9b43a1
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-06-04 23:14:50 -0300

    Add images for pypi caching and uwsgi server

---
+++ uwsgi/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /code
+
+if [ -n "$PYPI_PORT" ]; then
+	PYPI_FLAGS="-i ${PYPI_PORT/tcp:/http:}/root/pypi/"
+fi
+
+pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
+
+uwsgi -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
+
