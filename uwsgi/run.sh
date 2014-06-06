842b0d1531562c50f0d9943a92530b722b4cebb4
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-06-06 06:31:59 -0300

    Use the virtualenv, Luke
    Python images are now using virtualenvs for their dependencies. Although
    it's easy to install dependencies system wide with pip and docker, I was
    having problems with DistributionNotFound errors.
    
    The move to google/python is because of a weird bug with current
    debian:jessie|wheezy images that cause retrieval of HTTPS urls with
    python fail.

--- uwsgi/run.sh
+++ uwsgi/run.sh
@@ -6,7 +6,7 @@ if [ -n "$PYPI_PORT" ]; then
 	PYPI_FLAGS="-i ${PYPI_PORT/tcp:/http:}/root/pypi/"
 fi
 
-pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
+/env/bin/pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
 
-uwsgi -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
+/env/bin/uwsgi --virtualenv /env -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
 
