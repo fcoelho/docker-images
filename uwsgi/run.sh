cb4a8a91bc3fe0a327b137cb00b46eed792b41c5
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-06-08 21:32:24 -0300

    Create virtualenv during run time (not during build-time)

--- uwsgi/run.sh
+++ uwsgi/run.sh
@@ -6,6 +6,9 @@ if [ -n "$PYPI_PORT" ]; then
 	PYPI_FLAGS="-i ${PYPI_PORT/tcp:/http:}/root/pypi/"
 fi
 
+virtualenv /env
+/env/bin/pip install uwsgi
+
 /env/bin/pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
 
 /env/bin/uwsgi --virtualenv /env -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
