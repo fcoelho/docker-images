4edf6107fd235cea980176ef41910cf6f28c4214
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-06-09 03:23:26 -0300

    back to multiple volumes instead of a single /data

--- uwsgi/run.sh
+++ uwsgi/run.sh
@@ -4,13 +4,14 @@ if [ -n "$PYPI_PORT" ]; then
 	PYPI_FLAGS="-i ${PYPI_PORT/tcp:/http:}/root/pypi/"
 fi
 
-if find /data/env -maxdepth 0 -empty | read v; then
-	virtualenv /data/env
+if find /env -maxdepth 0 -empty | read v; then
+	virtualenv /env
 fi
 
-/data/env/bin/pip install uwsgi $PYPI_FLAGS
 
-/data/env/bin/pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
+/env/bin/pip install uwsgi $PYPI_FLAGS
 
-/data/env/bin/uwsgi --virtualenv /data/env --chdir /data/code -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
+cd /code && /env/bin/pip install -r ${REQUIREMENTS_FILE:-requirements.txt} $PYPI_FLAGS
+
+cd /code && /env/bin/uwsgi --virtualenv /env --chdir /code -w ${WSGI_MODULE:-wsgi:application} --uwsgi-socket 0.0.0.0:5000
 
