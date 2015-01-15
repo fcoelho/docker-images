d6a42e08a11fbd059b4ae01b4fe68709536ccc67
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2015-01-15 16:39:00 -0200

    Add config for upload limits
    This the upload limits for php to be changed via environment
    variables. The env var `MAX_UPLOAD_SIZE` should have the raw value to be
    used by php, which makes it possible to use values in raw number of
    bytes or values such as `32K`, `15M`, `2G`.

--- phpfpm/run.sh
+++ phpfpm/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -n "$MAX_UPLOAD_SIZE" ]; then
+	sed -i "s/upload_max_filesize =.*/upload_max_filesize = $MAX_UPLOAD_SIZE/g" /etc/php5/fpm/php.ini
+	sed -i "s/post_max_size =.*/post_max_size = $MAX_UPLOAD_SIZE/g" /etc/php5/fpm/php.ini
+fi
+
 perl >>/etc/php5/fpm/pool.d/www.conf <<EOF
 foreach my \$key (keys %ENV) {
 	next if \$key eq "_";
