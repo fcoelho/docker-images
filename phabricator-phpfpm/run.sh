7777f841b2917b882787bca7780ee2e1b5179659
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2015-02-12 14:03:59 -0200

    Set upload max size in Phabricator's config

--- phabricator-phpfpm/run.sh
+++ phabricator-phpfpm/run.sh
@@ -3,6 +3,8 @@
 if [ -n "$MAX_UPLOAD_SIZE" ]; then
 	sed -i "s/upload_max_filesize =.*/upload_max_filesize = $MAX_UPLOAD_SIZE/g" /etc/php5/fpm/php.ini
 	sed -i "s/post_max_size =.*/post_max_size = $MAX_UPLOAD_SIZE/g" /etc/php5/fpm/php.ini
+
+	/phabricator/phabricator/bin/config set storage.upload-size-limit "$MAX_UPLOAD_SIZE"
 fi
 
 perl >>/etc/php5/fpm/pool.d/www.conf <<EOF
