e832362b9802b005ddb278dffb9d24a91c1031f0
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2015-07-31 16:16:52 -0300

    Add filename for beat schedule database

--- celery/run-server.sh
+++ celery/run-server.sh
@@ -5,7 +5,7 @@ if [ -n "$CELERY_MODULE" ]; then
 fi
 
 if [ "$RUN_CELERY_BEAT" = "true" ]; then
-	CELERY_BEAT="-B"
+	CELERY_BEAT="-B -s /tmp/celerybeat-schedule"
 fi
 
 cd /code && /env/bin/celery $CELERY_APP $CELERY_BEAT -c ${CELERY_CONCURRENCY:-1} -l ${CELERY_LOG_LEVEL:-info} -f ${CELERY_LOG_FILE:-/log/celery.log} worker
